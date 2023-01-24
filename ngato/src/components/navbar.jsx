67d2848475cc17e6e500538eb804d2f9cdb9d27f
wongiro <sonrobin2018@gmail.com>
2023-01-24 18:22:16 +0300

    added all the components needed and the nav bar feature

---
+++ ngato/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import './navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='links'>
+            <Link to='/'> Shop </Link>
+            <Link to='/cart'><AiOutlineShoppingCart size={32}/></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
