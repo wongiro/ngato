57ee81a7ebc271c6fbd80f4537036eeb8af7e51b
wongiro <sonrobin2018@gmail.com>
2023-01-26 15:26:20 +0300

    made it possible to show the number of added items to the add to cart button

---
+++ ngato/src/pages/shop/Items.jsx
@@ -0,0 +1,44 @@
+import shoe from '../../assets/shoe.jpeg'
+import chels from '../../assets/chels.jpeg'
+import nike from '../../assets/nike.jpeg'
+import boots from '../../assets/boots.jpeg'
+
+export const Items = [
+    {
+        id: 1,
+        image: nike,
+        title: 'Vans',
+         price: 8000
+    },
+    {
+        id: 2,
+        image: nike,
+        title: 'Chelsea Boots',
+        price: 8000
+     
+    },
+    {
+        id: 3,
+        image: nike,
+        title: 'Nike Airmaxes',
+        price: 8000
+    },
+    {
+        id: 4,
+        image: nike,
+        title: 'Nike Airmaxes',
+        price: 8000
+    },
+    {
+        id: 5,
+        image: nike,
+        title: 'Nike Airmaxes',
+        price: 8000
+    },
+    {
+        id: 6,
+        image: nike,
+        title: 'Nike Air Zoom',
+        price: 8000
+    }
+]
